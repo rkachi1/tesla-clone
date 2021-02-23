22feb27c3a1f22aeaca78284c7110b30e3dd9e4c
rkachi1 <rangarajkk@gmail.com>
2021-02-23 00:28:05 -0500

    added react project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Tesla Home Page</h1>
+      <h1>Welcome to Tesla Home Page! Clever programmer rocks</h1>
     </div>
   );
 }
